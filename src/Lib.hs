349b82ac5c92c09b48f266893a9a2e892700fe21
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2023-03-13 10:23:51 +0900

    Get vmDir

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,13 @@ module Lib
     ( dispatcher
     ) where
 
+import System.Environment
+import System.FilePath
+
 dispatcher :: [String] -> IO ()
-dispatcher ["list"] = putStrLn "list"
+dispatcher ["list"] = do
+    home <- getEnv "HOME"
+    let vmDir = home </> "Virtual Machines.localized"
+    putStrLn vmDir
 dispatcher (cmd:_) = putStrLn $ "unknown command: " ++ cmd
 dispatcher _ = putStrLn $ "specify arguments"
