aaca481410fe70ca8a5881f6bf102cab48b135d6
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2023-03-13 21:17:40 +0900

    Add showVmList

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,16 @@ import System.Exit
 import System.Directory
 
 dispatcher :: [String] -> IO ()
-dispatcher ["list"] = do
+dispatcher ["list"] = showVmList
+dispatcher (cmd:_) = do
+    putStrLn $ "unknown command: " ++ cmd
+    exitWith (ExitFailure 1)
+dispatcher _ = do
+    putStrLn "specify arguments"
+    exitWith (ExitFailure 1)
+
+showVmList :: IO ()
+showVmList = do
     home <- getEnv "HOME"
     let vmDir = home </> "Virtual Machines.localized"
     dirs <- listDirectory vmDir
@@ -18,12 +27,6 @@ dispatcher ["list"] = do
     dirs'' <- mapM findVmxFiles dirs'
     let vmxFiles = concat dirs''
     putStr $ unlines vmxFiles
-dispatcher (cmd:_) = do
-    putStrLn $ "unknown command: " ++ cmd
-    exitWith (ExitFailure 1)
-dispatcher _ = do
-    putStrLn "specify arguments"
-    exitWith (ExitFailure 1)
 
 findVmxFiles :: FilePath -> IO [FilePath]
 findVmxFiles dir = do
