3153a1ff1c6004d57a0af32d420b736c5be82820
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2023-03-13 20:57:00 +0900

    Add findVmxFiles

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,15 +5,30 @@ module Lib
 import System.Environment
 import System.FilePath
 import System.Exit
+import System.Directory
 
 dispatcher :: [String] -> IO ()
 dispatcher ["list"] = do
     home <- getEnv "HOME"
     let vmDir = home </> "Virtual Machines.localized"
-    putStrLn vmDir
+    dirs <- listDirectory vmDir
+    -- print $ show dirs
+    let dirs' = map ((</>) vmDir) dirs
+    -- print $ show dirs'
+    dirs'' <- mapM findVmxFiles dirs'
+    let vmxFiles = concat dirs''
+    putStr $ unlines vmxFiles
 dispatcher (cmd:_) = do
     putStrLn $ "unknown command: " ++ cmd
     exitWith (ExitFailure 1)
 dispatcher _ = do
     putStrLn "specify arguments"
     exitWith (ExitFailure 1)
+
+findVmxFiles :: FilePath -> IO [FilePath]
+findVmxFiles dir = do
+    files <- getDirectoryContents dir
+    return $ map ((</>) dir) (filter isVmxFile files)
+
+isVmxFile :: FilePath -> Bool
+isVmxFile path = drop ((length path) -4) path == ".vmx"
