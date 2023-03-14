eddf16ff5fa36e87ad63baf96570951e1e829fbe
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2023-03-14 10:30:47 +0900

    Add startVm

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,9 +8,11 @@ import System.Exit
 import System.Directory
 import Data.List (find)
 import Data.Maybe
+import System.Process
 
 dispatcher :: [String] -> IO ()
 dispatcher ["list"] = showVmList
+dispatcher ("start":args) = startVm args
 dispatcher (cmd:_) = do
     putStrLn $ "unknown command: " ++ cmd
     exitWith (ExitFailure 1)
@@ -23,6 +25,19 @@ showVmList = do
     vmxFilePaths <- getVmxFilePaths
     putStr $ unlines $ let numbers = [0..] :: [Int]
         in zipWith (\n path -> show n ++ " - " ++ getVmNameFromVmxFilePath path) numbers vmxFilePaths
+
+startVm :: [String] -> IO ()
+startVm [numberString] = do
+    let number = read numberString :: Int
+    vmxFilePaths <- getVmxFilePaths
+    let vmxFilePath = vmxFilePaths !! number
+    let command = "vmrun start '" ++ vmxFilePath ++ "' gui"
+    putStrLn command
+    _ <- system command
+    return ()
+startVm _ = do
+    putStrLn "specify vm number"
+    exitWith (ExitFailure 1)
 
 getVmxFilePaths :: IO [FilePath]
 getVmxFilePaths = do
